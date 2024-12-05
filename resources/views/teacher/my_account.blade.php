a5f3524d09fb83c6e711f103a54a13b8aa0b7e06
Du-Xuan-Tung-Lam <titpiercing2002@gmail.com>
2024-12-05 18:43:35 +0700

    Update

--- resources/views/teacher/my_account.blade.php
+++ resources/views/teacher/my_account.blade.php
@@ -8,7 +8,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>My Profile</h1>
+            <h1>My Account</h1>
           </div>
     
         </div>
@@ -22,7 +22,8 @@
           <!-- left column -->
           <div class="col-md-12">
 
-               @include('_messages')  
+               @include('_message')
+               
             <div class="card card-primary">
               <form method="post" action="" enctype="multipart/form-data">
                  {{ csrf_field() }}
@@ -30,37 +31,43 @@
                   <div class="row">
                     <div class="form-group col-md-6">
                       <label>First Name <span style="color: red;">*</span></label>
-                      <input type="text" class="form-control" value="{{ old('name', $getRecord->first_name) }}" name="first_name" required placeholder="First Name">
-                      <div style="color:red">{{ $errors->first('first_name') }}</div>  
+                      <input type="text" class="form-control" value="{{ old('name', $getRecord->name) }}" name="name" required placeholder="First Name">
+                      <div style="color:red">{{ $errors->first('name') }}</div>
                     </div>  
 
                     <div class="form-group col-md-6">
                       <label>Last Name <span style="color: red;">*</span></label>
-                      <input type="text" class="form-control" value="{{ old('surname', $getRecord->last_name) }}" name="last_name" required placeholder="Last Name">
-                      <div style="color:red">{{ $errors->first('surname') }}</div>
+                      <input type="text" class="form-control" value="{{ old('last_name', $getRecord->last_name) }}" name="last_name" required placeholder="Last Name">
+                      <div style="color:red">{{ $errors->first('last_name') }}</div>
                     </div>  
+
+
+               
 
                     <div class="form-group col-md-6">
                       <label>Gender <span style="color: red;">*</span></label>
-                      <select class="form-control" required name="gender_id">
+                      <select class="form-control" required name="gender">
                           <option value="">Select Gender</option>
-                          <option {{ (old('gender_id', $getRecord->gender_id) == 'Male') ? 'selected' : '' }} value="Male">Male</option>  
-                          <option {{ (old('gender_id', $getRecord->gender_id) == 'Female') ? 'selected' : '' }} value="Female">Female</option>  
-                          <option {{ (old('gender_id', $getRecord->gender_id) == 'Other') ? 'selected' : '' }} value="Other">Other</option>  
+                          <option {{ (old('gender', $getRecord->gender) == 'Male') ? 'selected' : '' }} value="Male">Male</option>
+                          <option {{ (old('gender', $getRecord->gender) == 'Female') ? 'selected' : '' }} value="Female">Female</option>
+                          <option {{ (old('gender', $getRecord->gender) == 'Other') ? 'selected' : '' }} value="Other">Other</option>
                       </select>
-                      <div style="color:red">{{ $errors->first('gender_id') }}</div> 
+                      <div style="color:red">{{ $errors->first('gender') }}</div>
                     </div>  
 
-                    <div class="form-group col-md-6">
+
+                     <div class="form-group col-md-6">
                       <label>Date of Birth <span style="color: red;">*</span></label>
-                      <input type="date" class="form-control" required value="{{ old('dob', $getRecord->date_of_birth) }}" name="dob" >
-                      <div style="color:red">{{ $errors->first('dob') }}</div>
+                      <input type="date" class="form-control" required value="{{ old('date_of_birth', $getRecord->date_of_birth) }}" name="date_of_birth" >
+                      <div style="color:red">{{ $errors->first('date_of_birth') }}</div>
                     </div>  
+
+                 
 
                     <div class="form-group col-md-6">
                       <label>Mobile Number <span style="color: red;"></span></label>
-                      <input type="tel" class="form-control" value="{{ old('phone_number', $getRecord->mobile_number) }}" name="phone_number"  placeholder="Mobile Number"> 
-                      <div style="color:red">{{ $errors->first('phone_number') }}</div> 
+                      <input type="text" class="form-control" value="{{ old('mobile_number', $getRecord->mobile_number) }}" name="mobile_number"  placeholder="Mobile Number">
+                      <div style="color:red">{{ $errors->first('mobile_number') }}</div>
                     </div> 
 
                     <div class="form-group col-md-6">
@@ -69,19 +76,23 @@
                       <div style="color:red">{{ $errors->first('marital_status') }}</div>
                     </div> 
 
+                    
+
+                 
+
                     <div class="form-group col-md-6">
-                      <label>Profile Picture  <span style="color: red;"></span></label>
-                      <input type="file" class="form-control" name="profile_picture" >  
-                      <div style="color:red">{{ $errors->first('profile_picture') }}</div>  
-                        @if(!empty($getRecord->getProfileImage()))
-                        <img src="{{  $getRecord->getProfileImage() }}" style="width: auto;height: 60px;">  
+                      <label>Profile Pic <span style="color: red;"></span></label>
+                      <input type="file" class="form-control" name="profile_pic" >
+                      <div style="color:red">{{ $errors->first('profile_pic') }}</div>
+                        @if(!empty($getRecord->getProfile()))
+                        <img src="{{  $getRecord->getProfile() }}" style="width: auto;height: 50px;"> 
                       @endif
                     </div> 
 
                      <div class="form-group col-md-6">
                       <label>Current Address  <span style="color: red;">*</span></label>
-                      <textarea class="form-control" name="current_address" required>{{ old('current_address', $getRecord->address) }}</textarea>  
-                      <div style="color:red">{{ $errors->first('current_address') }}</div>
+                      <textarea class="form-control" name="address" required>{{ old('address', $getRecord->address) }}</textarea>
+                      <div style="color:red">{{ $errors->first('address') }}</div>
                     </div> 
 
                     <div class="form-group col-md-6">
@@ -89,6 +100,7 @@
                       <textarea class="form-control" name="permanent_address" >{{ old('permanent_address', $getRecord->permanent_address) }}</textarea>
                       <div style="color:red">{{ $errors->first('permanent_address') }}</div>
                     </div> 
+
 
                     <div class="form-group col-md-6">
                       <label>Qualification  <span style="color: red;"></span></label>
@@ -102,22 +114,26 @@
                       <div style="color:red">{{ $errors->first('work_experience') }}</div>
                     </div> 
 
+ 
+
                   </div>
 
                   <hr />
 
 
+                  
                   <div class="form-group">
                     <label>Email <span style="color: red;">*</span></label>
-                    <input type="email" class="form-control" name="user_email" value="{{ old('email', $getRecord->email) }}" required placeholder="Email">
-                    <div style="color:red">{{ $errors->first('user_email') }}</div>  
+                    <input type="email" class="form-control" name="email" value="{{ old('email', $getRecord->email) }}" required placeholder="Email">
+                    <div style="color:red">{{ $errors->first('email') }}</div>
                   </div>
+                
                 
                 </div>
                 <!-- /.card-body -->
 
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Save Changes</button> 
+                  <button type="submit" class="btn btn-primary">Update</button>
                 </div>
               </form>
             </div>
