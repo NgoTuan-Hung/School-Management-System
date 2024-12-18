6811f7907d266b6e0c5777b944a427f030a19381
danh2422001 <danh2422001@gmail.com>
2024-12-18 14:25:14 +0700

    fixed bug

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -223,6 +223,25 @@
         </li>
 
         <li class="nav-item">
+          <a href="{{ url('student/my_subject') }}" class="nav-link @if(Request::segment(2) == 'my_subject') active @endif">
+            <i class="nav-icon far fa-user"></i>
+            <p>
+              My Subject
+            </p>
+          </a>
+        </li>
+
+
+         <li class="nav-item">
+          <a href="{{ url('student/my_timetable') }}" class="nav-link @if(Request::segment(2) == 'my_timetable') active @endif">
+            <i class="nav-icon far fa-user"></i>
+            <p>
+              My Timetable
+            </p>
+          </a>
+        </li>
+
+        <li class="nav-item">
           <a href="{{ url('student/account') }}" class="nav-link @if(Request::segment(2) == 'account') active @endif">
             <i class="nav-icon far fa-user"></i>
             <p>
@@ -252,24 +271,7 @@
         </a>
       </li>
 
-      <li class="nav-item">
-        <a href="{{ url('student/my_subject') }}" class="nav-link @if(Request::segment(2) == 'my_subject') active @endif">
-          <i class="nav-icon far fa-user"></i>
-          <p>
-            My Subject
-          </p>
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a href="{{ url('student/my_timetable') }}" class="nav-link @if(Request::segment(2) == 'my_timetable') active @endif">
-          <i class="nav-icon far fa-user"></i>
-          <p>
-            My Timetable
-          </p>
-        </a>
-      </li>
-
+      
       <li class="nav-item">
         <a href="{{url('parent/change_password') }}" class="nav-link @if(Request::segment(2)=='change_password') active @endif">
           <i class="nav-icon far fa-user"></i>
