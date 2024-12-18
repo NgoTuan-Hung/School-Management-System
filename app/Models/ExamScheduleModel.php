523dd6531b5cdbf2ef52fc96b1f4e24b91f0a4ea
baong080921 <bao992001pth@gmail.com>
2024-12-18 12:30:09 +0700

    update

--- app/Models/ExamScheduleModel.php
+++ app/Models/ExamScheduleModel.php
@@ -82,5 +82,10 @@ class ExamScheduleModel extends Model
                 ->get();
     }
 
+    static public function getMark($student_id, $exam_id, $class_id, $subject_id)
+    {
+        return MarksRegisterModel::CheckAlreadyMark($student_id, $exam_id, $class_id, $subject_id);
+    }
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -167,6 +167,21 @@ Route::group(['middleware' => 'admin'], function(){
     Route::get('admin/examinations/exam_schedule', [ExaminationsController::class, 'exam_schedule']); 
     Route::post('admin/examinations/exam_schedule_insert', [ExaminationsController::class, 'exam_schedule_insert']); 
 
+
+    Route::get('admin/examinations/marks_register', [ExaminationsController::class, 'marks_register']); 
+    Route::post('admin/examinations/submit_marks_register', [ExaminationsController::class, 'submit_marks_register']); 
+    Route::post('admin/examinations/single_submit_marks_register', [ExaminationsController::class, 'single_submit_marks_register']); 
+
+    Route::get('admin/examinations/marks_grade', [ExaminationsController::class, 'marks_grade']);
+    Route::get('admin/examinations/marks_grade/add', [ExaminationsController::class, 'marks_grade_add']);
+    Route::post('admin/examinations/marks_grade/add', [ExaminationsController::class, 'marks_grade_insert']);
+    Route::get('admin/examinations/marks_grade/edit/{id}', [ExaminationsController::class, 'marks_grade_edit']);
+    Route::post('admin/examinations/marks_grade/edit/{id}', [ExaminationsController::class, 'marks_grade_update']);
+    Route::get('admin/examinations/marks_grade/delete/{id}', [ExaminationsController::class, 'marks_grade_delete']);
+    
+
+
+
     //change password url:
     Route::get('admin/change_password',[UserController::class,'change_password']);
     Route::post('admin/change_password',[UserController::class,'update_change_password']);
